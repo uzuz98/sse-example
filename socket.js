7291a27a4c1272dfcc7782c5b7a9157203c88efa
ThanhHlb <thanhhlb@coin98.finance>
2024-11-14 09:40:24 +0700

    chore: fix socket

--- socket.js
+++ socket.js
@@ -2,7 +2,6 @@ const { createServer } = require('http')
 const { Server } = require('socket.io')
 const express = require('express')
 const cors = require('cors')
-const jwt = require('jsonwebtoken')
 
 const eventName = {
   connectWallet: 'connect-wallet',
